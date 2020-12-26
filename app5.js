c3be47600148c5532abf8672555c4c2cdb426010
lemon <eunbiryung@gmail.com>
2020-12-26 19:47:40 +0900

    mysql 연동한 로그인 하기

--- app5.js
+++ app5.js
@@ -54,7 +54,7 @@ function connectDB() {
             if(inSalt) {
                 return crypto.createHmac('sha1', inSalt).update(plainText).digest('hex');
             } else {
-                return crypto.createHmac('sha1', this.salt).update(plainText).digest('hext')
+                return crypto.createHmac('sha1', this.salt).update(plainText).digest('hex')
             };
         });
 
